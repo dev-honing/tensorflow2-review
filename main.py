9b53536149c1f53840087878554e13a1e76746da
Ho Nyeong <dev.honing@gmail.com>
2024-03-25 16:54:22 +0900

    Feat: Keras API로 서브 클래스 방식의 모델링

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = x_train[..., tf.newaxis].astype("float32")
 x_test = x_test[..., tf.newaxis].astype("float32")
 
-# 5. 데이터셋 섞기 및 배치 설정
+#* 5. 데이터셋 섞기 및 배치 설정
 # 섞기
 train_ds = tf.data.Dataset.from_tensor_slices(
     (x_train, y_train)).shuffle(10000).batch(32)
@@ -31,3 +31,26 @@ train_ds = tf.data.Dataset.from_tensor_slices(
 
 # 배치 설정
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+#* 6. 모델링(서브 클래스 방식) - Keras API로 커스텀 모델 정의
+class MyModel(Model):
+  # 모델의 레이어를 초기화
+  def __init__(self):
+    # 부모 클래스의 생성자를 호출
+    super(MyModel, self).__init__()
+    # 레이어 정의(Conv2D, Flatten, Dense)
+    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.d1 = Dense(128, activation='relu')
+    self.d2 = Dense(10)
+
+  # 모델의 실행 메서드 정의(순전파)
+  def call(self, x):
+    # 입력 데이터를 레이어에 순차적으로 전달
+    x = self.conv1(x)
+    x = self.flatten(x)
+    x = self.d1(x)
+    return self.d2(x)
+
+# 모델 객체 생성
+model = MyModel()
