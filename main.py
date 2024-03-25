f825788ab7bdf7ff67f35f50af038ce2d57e2bcf
Ho Nyeong <dev.honing@gmail.com>
2024-03-25 15:55:24 +0900

    Feat: MNIST 데이터셋 다운로드 및 변수 할당

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # main.py
 
+# 1. Tensorflow2 설치 및 버전 정보 확인
 import tensorflow as tf
+# print("TensorFlow version:", tf.__version__) # TensorFlow version: 2.16.1
 
-print("TensorFlow version:", tf.__version__) # TensorFlow version: 2.16.1
+# 2. Tensorflow2 MNIST 데이터셋 다운로드
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+mnist = tf.keras.datasets.mnist # keras 데이터셋 중에서 MNIST 데이터셋을 변수에 할당
